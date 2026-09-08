40b692ee8c569dc6cee81cc796c8dd16d72e9836
humaiarad <d.humaiara@gmail.com>
2026-09-08 11:43:51 -0400

    adding book class

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        self.current_page = 1
+
+    def turn_page(self, page_count):
+        if not isinstance(page_count, int):
+            raise TypeError("page_count must be an integer")
+        
+        if self.current_page < self.page_count:
+            self.current_page += 1
+            print("Flipping the page...wow, you read fast!")
+        else:
+            print("You are already on the last page.")
+            
     
         
