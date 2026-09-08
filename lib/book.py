67a26353990e434cd0787bdaedae0b81a168e5c0
humaiarad <d.humaiara@gmail.com>
2026-09-08 12:06:28 -0400

    class book and coffee is done

--- lib/book.py
+++ lib/book.py
@@ -3,18 +3,14 @@
 class Book:
     def __init__(self, title, page_count):
         self.title = title
-        self.page_count = page_count
-        self.current_page = 1
-
-    def turn_page(self, page_count):
         if not isinstance(page_count, int):
-            raise TypeError("page_count must be an integer")
-        
-        if self.current_page < self.page_count:
-            self.current_page += 1
-            print("Flipping the page...wow, you read fast!")
+            print("page_count must be an integer")
         else:
-            print("You are already on the last page.")
+            self.page_count = page_count
+
+    def turn_page(self):     
+        print("Flipping the page...wow, you read fast!")
+        
             
     
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):        
+        size_options = ["Small", "Medium", "Large"]
+        if size not in size_options:
+            print("size must be Small, Medium, or Large.")
+        else:
+            self.size = size
+
+        self.price = price
+
+    def tip(self):
+        self.price += 1
+        print("This coffee is great, here's a tip!")
